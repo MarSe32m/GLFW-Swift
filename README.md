8f1a1e5c646e72affc74fe2fd5610a6d032f079c
Sebastian Toivonen <sebastian.toivonen@gmail.com>
2020-09-26 11:07:29 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# glfw-swift
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import glfw_swiftTests
+
+var tests = [XCTestCaseEntry]()
+tests += glfw_swiftTests.allTests()
+XCTMain(tests)
