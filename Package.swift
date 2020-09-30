518c924c948ab1284628fa3117feabcca5726da6
Sebastian Toivonen <sebastian.toivonen@gmail.com>
2020-09-30 16:49:54 +0300

    Break out C code to own module, refactor to remove glad_ prefixes

--- Package.swift
+++ Package.swift
@@ -16,19 +16,15 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GLFWSwift",
-            dependencies: [],
-            cSettings: [.define("_GLFW_WIN32", .when(platforms: [.windows]))],
-            linkerSettings: [
+        .target(name: "CGLFW",
+                cSettings: [.define("_GLFW_WIN32", .when(platforms: [.windows]))],
+                linkerSettings: [
                         .linkedLibrary("User32"),
                         .linkedLibrary("ComCtl32"),
                         .linkedLibrary("OpenGL32"),
             ]),
-        .testTarget(
-            name: "GLFWSwiftTests",
-            dependencies: ["GLFWSwift"]),
+        .target(
+            name: "GLFWSwift",
+            dependencies: ["CGLFW"])
     ]
 )
